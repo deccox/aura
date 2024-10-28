c3f21c7d7dfa61c616f9d8469c0869513e381341
deccox <josekayna13@gmail.com>
2024-10-28 14:50:10 -0300

    fix: localsotre

--- public/js/handlerClick.js
+++ public/js/handlerClick.js
@@ -1,6 +1,25 @@
 $(function() {
 
 
+    (function checkReffer(){
+        if (document.referrer.includes('/checkout')){
+            
+            sessionStorage.clear();
+            localStorage.clear();
+            $('.product-items').remove();
+            $('<div class="product-items"></div>').insertBefore('.product-footer');
+
+            
+            
+        }
+    })()
+
+
+
+ 
+
+    
+
 
     const products = [
         {
@@ -49,7 +68,6 @@ $(function() {
 
     $('.header-img').on('click', function(){
         if(window.location.pathname !== '/'){
-            window.location.reload()
             window.location.href = '/';
   
 
@@ -132,208 +150,219 @@ $(function() {
     }
     
 
-    
-
-    $('.buy-btn').on('click', (e) => {
-        let i = $(e.target).attr('data-product');
-        let prod = products[i - 1];
+    (function allConfig(){
         
-        let produto = {
-            id: prod.id,
-            nome: prod.nome,
-            img: prod.img,
-            price: prod.price,
-            quantity: 1,
-            amount: prod.price
-        };
-    
-        // Recupera os produtos existentes do localStorage ou inicializa um array vazio
-        let prods = JSON.parse(localStorage.getItem('produtos')) || [];
-    
-        if ($('.product-items [data-product="' + prod.id + '"]').length) {
-            // Produto já existe, incrementa a quantidade
-            let a = $('.product-items [data-product="' + prod.id + '"] .product-item-amount div:nth-of-type(2)');
-            let item = $('.product-items [data-product="' + prod.id + '"] .product-item-totalprice');
-            let val = Number(a.text());
+
+            $('.buy-btn').on('click', (e) => {
+                let i = $(e.target).attr('data-product');
+                let prod = products[i - 1];
+                
+                let produto = {
+                    id: prod.id,
+                    nome: prod.nome,
+                    img: prod.img,
+                    price: prod.price,
+                    quantity: 1,
+                    amount: prod.price
+                };
             
-            val += 1;  // Incrementa a quantidade
-            let priceItem = Number(prod.price);
+                // Recupera os produtos existentes do localStorage ou inicializa um array vazio
+                let prods = JSON.parse(localStorage.getItem('produtos')) || [];
             
-            item.attr('data-amount', priceItem * val);
-            item.text(`R$ ${priceItem * val},00`);
+                if ($('.product-items [data-product="' + prod.id + '"]').length) {
+                    // Produto já existe, incrementa a quantidade
+                    let a = $('.product-items [data-product="' + prod.id + '"] .product-item-amount div:nth-of-type(2)');
+                    let item = $('.product-items [data-product="' + prod.id + '"] .product-item-totalprice');
+                    let val = Number(a.text());
+                    
+                    val += 1;  // Incrementa a quantidade
+                    let priceItem = Number(prod.price);
+                    
+                    item.attr('data-amount', priceItem * val);
+                    item.text(`R$ ${priceItem * val},00`);
+                    
+                    a.text(val);
+                    a.attr('data-quantity', val);
+                    
+                    // Atualiza o objeto do produto
+                    produto.quantity = val;
+                    produto.amount = priceItem * val;
             
-            a.text(val);
-            a.attr('data-quantity', val);
-            
-            // Atualiza o objeto do produto
-            produto.quantity = val;
-            produto.amount = priceItem * val;
-    
-            
-        } else {
-            // Produto não existe, adiciona um novo
-            $('.product-items').append(`
-                <div data-product="${prod.id}" class="product-item">
-                    <div class="product-delete">
-                        <i class="fa-solid fa-trash"></i>
-                    </div>
-                    <div class="product-item-img">
-                        <img src="${prod.img}" alt="">
-                    </div>
-                    <div class="product-item-desc">
-                        <div class="product-item-title">
-                            ${prod.nome}
-                        </div>
-                        <div class="product-item-price" data-price="${prod.price}">
-                            <div class="product-item-amount">
-                                <div class="increase-btn">+</div>
-                                <div class="quantity" value="1" data-quantity="1">1</div>
-                                <div class="decrease-btn">-</div>
+                    
+                } else {
+                    // Produto não existe, adiciona um novo
+                    $('.product-items').append(`
+                        <div data-product="${prod.id}" class="product-item">
+                            <div class="product-delete">
+                                <i class="fa-solid fa-trash"></i>
                             </div>
-                            <div class="product-item-totalprice" data-amount="${prod.price}">
-                                R$ ${prod.price},00
+                            <div class="product-item-img">
+                                <img src="${prod.img}" alt="">
+                            </div>
+                            <div class="product-item-desc">
+                                <div class="product-item-title">
+                                    ${prod.nome}
+                                </div>
+                                <div class="product-item-price" data-price="${prod.price}">
+                                    <div class="product-item-amount">
+                                        <div class="increase-btn">+</div>
+                                        <div class="quantity" value="1" data-quantity="1">1</div>
+                                        <div class="decrease-btn">-</div>
+                                    </div>
+                                    <div class="product-item-totalprice" data-amount="${prod.price}">
+                                        R$ ${prod.price},00
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
-            `);
-    
-            // Adiciona o novo produto ao array `prods`
-            prods[prod.id - 1] = produto;
-    
-            // Vincula os eventos de "+" e "-" após adicionar o item
-            attachEventHandlers(prod.id);
-        }
-    
-        // Salva o array `prods` atualizado no localStorage
-        localStorage.setItem('produtos', JSON.stringify(prods));
+                    `);
+            
+                    // Adiciona o novo produto ao array `prods`
+                    prods[prod.id - 1] = produto;
+            
+                    // Vincula os eventos de "+" e "-" após adicionar o item
+                    attachEventHandlers(prod.id);
+                }
+            
+                // Salva o array `prods` atualizado no localStorage
+                localStorage.setItem('produtos', JSON.stringify(prods));
+                
+                updateConfiguration();
+                updateTotalItems();
+            });
+            
+            function attachEventHandlers(productId) {
+                // Incrementar quantidade
+                let prod = products[productId - 1];
+                $(`.product-items [data-product="${productId}"] .increase-btn`).on('click', function() {
+                    let quantityElem = $(this).siblings('.quantity');
+                    let currentQty = Number(quantityElem.attr('data-quantity'));
+            
+                    currentQty += 1;
+                    quantityElem.attr('data-quantity', currentQty);
+                    quantityElem.text(currentQty);
+            
+                    // Atualiza o preço total e a quantidade de itens
+                    updateTotalPrice(productId, currentQty);
+                    updateTotalItems(prod);
+                });
+            
+                // Decrementar quantidade
+                $(`.product-items [data-product="${productId}"] .decrease-btn`).on('click', function() {
         
-        updateConfiguration();
-        updateTotalItems();
-    });
-    
-    function attachEventHandlers(productId) {
-        // Incrementar quantidade
-        let prod = products[productId - 1];
-        $(`.product-items [data-product="${productId}"] .increase-btn`).on('click', function() {
-            let quantityElem = $(this).siblings('.quantity');
-            let currentQty = Number(quantityElem.attr('data-quantity'));
-    
-            currentQty += 1;
-            quantityElem.attr('data-quantity', currentQty);
-            quantityElem.text(currentQty);
-    
-            // Atualiza o preço total e a quantidade de itens
-            updateTotalPrice(productId, currentQty);
-            updateTotalItems(prod);
-        });
-    
-        // Decrementar quantidade
-        $(`.product-items [data-product="${productId}"] .decrease-btn`).on('click', function() {
-
-
+        
+                    
+                    let quantityElem = $(this).siblings('.quantity');
+                    let currentQty = Number(quantityElem.attr('data-quantity'));
             
-            let quantityElem = $(this).siblings('.quantity');
-            let currentQty = Number(quantityElem.attr('data-quantity'));
-    
-            if (currentQty > 1) {
-                currentQty -= 1;
+                    if (currentQty > 1) {
+                        currentQty -= 1;
+                    }
+            
+                    quantityElem.attr('data-quantity', currentQty);
+                    quantityElem.text(currentQty);
+            
+                    // Atualiza o preço total e a quantidade de itens
+                    updateTotalPrice(productId, currentQty);
+                    updateTotalItems();
+                });
             }
-    
-            quantityElem.attr('data-quantity', currentQty);
-            quantityElem.text(currentQty);
-    
-            // Atualiza o preço total e a quantidade de itens
-            updateTotalPrice(productId, currentQty);
-            updateTotalItems();
-        });
-    }
-    
-    function updateTotalPrice(productId, quantity) {
-        let productElem = $(`.product-items [data-product="${productId}"]`);
-        let price = Number(productElem.find('.product-item-price').attr('data-price'));
-    
-        let totalPrice = price * quantity;
-        productElem.find('.product-item-totalprice').text(`R$ ${totalPrice},00`);
-        $('.product-totalprice span:nth-of-type(2)').text(`R$ ${totalPrice},00`);
-        $('.header-car-info span:nth-of-type(2)').text(`R$ ${totalPrice},00`);
-    }
-    
-    function updateTotalItems() {
+            
+            function updateTotalPrice(productId, quantity) {
 
-   
-        let totalItems = 0;
-        let totalPrice = 0;
-        let totalQuantity = 0;
+                let produtos = JSON.parse(localStorage.getItem('produtos')) || [];
+
+
+                let productElem = $(`.product-items [data-product="${productId}"]`);
+                let price = Number(productElem.find('.product-item-price').attr('data-price'));
+            
+                let totalPrice = price * quantity;
+                productElem.find('.product-item-totalprice').text(`R$ ${totalPrice},00`);
+                $('.product-totalprice span:nth-of-type(2)').text(`R$ ${totalPrice},00`);
+                $('.header-car-info span:nth-of-type(2)').text(`R$ ${totalPrice},00`);
+
+                produtos[productId - 1].amount = totalPrice;
+                produtos[productId - 1].quantity = quantity;
+
+                localStorage.setItem('produtos', JSON.stringify(produtos))
+            }
+            
+            function updateTotalItems() {
         
-
-
-
-
-        $('.product-item-amount .quantity').each(function() {
-            totalItems += Number($(this).attr('data-quantity'));
-            
-
-        });
-
-
-        $('.product-items .product-item-price').each(function(){
-
-            let quantity = $(this).find('.quantity').attr('data-quantity')
-            let price = $(this).attr('data-price')
-
-            let tot = Number(price) * Number(quantity);
-
-            totalPrice += tot;
-            totalQuantity += quantity
-
-        })
-
-
-      
-
-        $('.product-totalprice span:nth-of-type(2)').text(`R$ ${totalPrice},00`)
-        $('.header-car-info span:nth-of-type(2)').text(`R$ ${totalPrice},00`)
-
-        // Atualiza o valor no span.car-qtn
-        $('.car-qtn').attr('value', totalItems);
-        $('.car-qtn').text(totalItems);
-
-        
-    }
-
-
-
-
-    function updateConfiguration(){
-        $('.product-delete').on('click', function(e) {
-            // Mostra o loader
-            
-
-
-            loader();
-            
-            // Remove o item e atualiza os totais
-            this.parentElement.remove();
-            updateTotalItems();
-            
            
-        });
-    }
-
-
-    const loader = () => {
-        $('body').append(`<div id="loader" class="loader"></div>`)
+                let totalItems = 0;
+                let totalPrice = 0;
+                let totalQuantity = 0;
+                
         
         
-        setTimeout(function() {
-            $('.loader').remove();
-        }, 1000);
-
-
         
-    }
-
+        
+                $('.product-item-amount .quantity').each(function() {
+                    totalItems += Number($(this).attr('data-quantity'));
+                    
+        
+                });
+        
+        
+                $('.product-items .product-item-price').each(function(){
+        
+                    let quantity = $(this).find('.quantity').attr('data-quantity')
+                    let price = $(this).attr('data-price')
+        
+                    let tot = Number(price) * Number(quantity);
+        
+                    totalPrice += tot;
+                    totalQuantity += quantity
+        
+                })
+        
+        
+              
+        
+                $('.product-totalprice span:nth-of-type(2)').text(`R$ ${totalPrice},00`)
+                $('.header-car-info span:nth-of-type(2)').text(`R$ ${totalPrice},00`)
+        
+                // Atualiza o valor no span.car-qtn
+                $('.car-qtn').attr('value', totalItems);
+                $('.car-qtn').text(totalItems);
+        
+                
+            }
+        
+        
+        
+        
+            function updateConfiguration(){
+                $('.product-delete').on('click', function(e) {
+                    // Mostra o loader
+                    
+        
+        
+                    loader();
+                    
+                    // Remove o item e atualiza os totais
+                    this.parentElement.remove();
+                    updateTotalItems();
+                    
+                   
+                });
+            }
+        
+        
+            const loader = () => {
+                $('body').append(`<div id="loader" class="loader"></div>`)
+                
+                
+                setTimeout(function() {
+                    $('.loader').remove();
+                }, 1000);
+        
+        
+                
+            }
+        
+    }())
 
 
 
@@ -352,7 +381,7 @@ $(function() {
 
 
     //checkout
-    (function initCheckout(){
+    function initCheckout(){
         let produtos = JSON.parse(localStorage.getItem('produtos')) || [];
         console.log(produtos)
 
@@ -390,6 +419,14 @@ $(function() {
 
 
         $('.pedido-total-item:nth-of-type(1) .pedido-rigth, .pedido-total-item:nth-of-type(3) .pedido-rigth').text(`R$ ${tot},00`)
-    }())                  
+    };   
+    
+    
+    (function(){
+  
+        if (window.location.pathname == '/checkout'){
+            initCheckout()
+        }
+    })();
 
 })
